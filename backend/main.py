a96ef61a4efb341be114dc64c5f92782ee618c77
nikhil chouksey <nikhilranger18@gmail.com>
2025-09-03 11:39:22 +0530

    backend bug fixes

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware, 
-  allow_origins = ["https://qr-code-generator-beryl.vercel.app/"], 
+  allow_origins = ["*"], 
   allow_credentials = True , 
   allow_headers = ["*"], 
   allow_methods = ["*"], 
